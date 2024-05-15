90ae7217141a5069750acdaa61e80465deb6a60e
Yana <yanakudrinskaya86@yandex.ru>
2024-05-15 15:36:55 +0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _health;
+    [SerializeField] private List<Weapon> _weapon;
+    [SerializeField] private Transform _shootPoint;
+
+    private Weapon _currentWeapon;
+    private int _currentHealth;
+    private Animator _animator;
+
+    public int Money { get; private set; }
+
+    private void Start()
+    {
+        _currentWeapon = _weapon[0];
+        _currentHealth = _health;
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            _currentWeapon.Shoot(_shootPoint);
+        }
+            
+
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        _currentHealth -= damage;
+        if(_currentHealth <=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnEnemyDied(int reward)
+    {
+        Money += reward;
+    }
+}
